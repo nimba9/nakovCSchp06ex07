9c7419b307726669678df273fe4a35381971fe03
vancisin peter <vancisin.peter@gmail.com>
2017-04-12 21:50:24 +0200

    Add project files.

---
+++ nakovCSchp06ex07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nakovCSchp06ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cardRank = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+            string [] colors = { "Spades", "Hearts", "Clubs", "Diamonds" };
+
+            
+            foreach (var rank in cardRank)
+            {
+                foreach (var color in colors)
+                {
+                    Console.WriteLine(rank + " of " + color);
+                }
+            }
+
+        }
+
+     }
+ }
+
